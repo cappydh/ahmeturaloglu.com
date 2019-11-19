dcf942897d84d8a8e0215c9c93bf8b0f014bcffa
cappydh <ahmeturaloglu@msn.com>
2019-11-19 21:36:24 +0300

    Updates

---
+++ precache-manifest.8f2f9dc47ac9f333999618d7f09c1d80.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76bd1c88a6354ac0d9628ce9c8450162",
+    "url": "/ahmeturaloglu.com/index.html"
+  },
+  {
+    "revision": "46536cb81fa043a0cecc",
+    "url": "/ahmeturaloglu.com/static/css/main.2d92fdd5.chunk.css"
+  },
+  {
+    "revision": "ab757ae49a3be0349921",
+    "url": "/ahmeturaloglu.com/static/js/2.bd02a25f.chunk.js"
+  },
+  {
+    "revision": "46536cb81fa043a0cecc",
+    "url": "/ahmeturaloglu.com/static/js/main.c4a82dc7.chunk.js"
+  },
+  {
+    "revision": "fce49372a3eb10929fae",
+    "url": "/ahmeturaloglu.com/static/js/runtime-main.fc362b66.js"
+  },
+  {
+    "revision": "415a26d34b9dfd28cfa70b641f2f8cdc",
+    "url": "/ahmeturaloglu.com/static/media/20833464.415a26d3.png"
+  },
+  {
+    "revision": "575e4317521b381ac94c0c8207c81979",
+    "url": "/ahmeturaloglu.com/static/media/Raleway-Bold.575e4317.ttf"
+  },
+  {
+    "revision": "580d0778ad254335be45bf58bb449f43",
+    "url": "/ahmeturaloglu.com/static/media/Raleway-Regular.580d0778.ttf"
+  },
+  {
+    "revision": "17ba6410cbc694808961a988fd4426de",
+    "url": "/ahmeturaloglu.com/static/media/Raleway-SemiBold.17ba6410.ttf"
+  }
+]);
